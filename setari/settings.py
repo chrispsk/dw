c773c5a4dba95757a179b76557c16f3d4ee3a395
chrispsk <chrispsk@gmail.com>
2026-06-19 19:15:56 +0100

    Clear SECRET_KEY in settings.py
    Removed the secret key for security reasons.

--- setari/settings.py
+++ setari/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '71@8@%(mlqe$qrja-&v!cxa4p!*t*jn*v#cc4xl6@e4at5&5=q'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
